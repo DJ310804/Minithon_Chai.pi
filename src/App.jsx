c630b4572e9f5ef0accd50f3a34b7a4b03dc1d94
anurag-nair16 <anuragnair1607.com>
2024-09-20 11:00:01 +0530

    transport component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import Page1 from './components/Page1';
 import Profile from './components/Profile';
 import Login from './components/Login';
+import Transport from './components/Transport';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="page1" element={<Page1 />} />
         <Route path="login" element={<Login />} />
         <Route path="profile" element={<Profile />} />
+        <Route path="transport" element={<Transport />} /> 
       </Route>
     </Routes>
   );
