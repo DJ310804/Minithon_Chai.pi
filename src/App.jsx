cf7913507480845658dd768c952fc8fe84d4ef4a
AniBande <aniket.bande22@spit.ac.in>
2024-09-20 11:24:11 +0530

    landing page ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Page1 from './components/Page1';
+// import Page1 from './components/Page1';
 import Profile from './components/Profile';
 import Login from './components/Login';
+import Page2 from './components/page2';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="page1" element={<Page1 />} />
+        {/* <Route path="page1" element={<Page1 />} /> */}
+        <Route path="page2" element={<Page2 />} />
         <Route path="login" element={<Login />} />
         <Route path="profile" element={<Profile />} />
       </Route>
